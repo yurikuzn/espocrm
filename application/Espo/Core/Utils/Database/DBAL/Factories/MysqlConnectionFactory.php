604063c1869d9c0949b818029683d1901276310a
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-02-01 17:07:19 +0200

    dev

--- application/Espo/Core/Utils/Database/DBAL/Factories/MysqlConnectionFactory.php
+++ application/Espo/Core/Utils/Database/DBAL/Factories/MysqlConnectionFactory.php
@@ -30,38 +30,19 @@
 namespace Espo\Core\Utils\Database\DBAL\Factories;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver\PDO\MySQL\Driver as PDOMySQLDriver;
 use Doctrine\DBAL\Exception as DBALException;
-use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\VersionAwarePlatformDriver as Driver;
-use Espo\Core\Utils\Config;
+
 use Espo\Core\Utils\Database\DBAL\ConnectionFactory;
-use Espo\Core\Utils\Database\DBAL\Driver\PDO\MySQL\Driver as PDOMySQLDriver;
 use Espo\ORM\DatabaseParams;
 use Espo\ORM\PDO\Options as PdoOptions;
 
 use PDO;
-use ReflectionClass;
-use RuntimeException;
 
 class MysqlConnectionFactory implements ConnectionFactory
 {
-    /** @var array<string, class-string<Driver>> */
-    private $driverClassNameMap = [
-        'mysqli' => 'Doctrine\\DBAL\\Driver\\Mysqli\\Driver', // @todo Revise usage.
-        'pdo_mysql' => PDOMySQLDriver::class,
-    ];
-
-    /** @var array<string, class-string<AbstractPlatform>> */
-    private $customPlatformClassNameMap = [
-        'MariaDb1027Platform' => 'Espo\\Core\\Utils\\Database\\DBAL\\Platforms\\MariaDb1027Platform',
-        'MySQL57Platform' => 'Espo\\Core\\Utils\\Database\\DBAL\\Platforms\\MySQL57Platform',
-        'MySQL80Platform' => 'Espo\\Core\\Utils\\Database\\DBAL\\Platforms\\MySQL80Platform',
-        'MySQLPlatform' => 'Espo\\Core\\Utils\\Database\\DBAL\\Platforms\\MySQLPlatform',
-    ];
-
     public function __construct(
-        private PDO $pdo,
-        private Config $config
+        private PDO $pdo
     ) {}
 
     /**
@@ -69,13 +50,13 @@ class MysqlConnectionFactory implements ConnectionFactory
      */
     public function create(DatabaseParams $databaseParams): Connection
     {
-        $driver = $this->createDriver();
+        $driver = new PDOMySQLDriver();
 
         $version = $this->getFullDatabaseVersion() ?? '';
         $platform = $driver->createDatabasePlatformForVersion($version);
 
         $params = [
-            'platform' => $this->handlePlatform($platform),
+            'platform' => $platform,
             'host' => $databaseParams->getHost(),
             'port' => $databaseParams->getPort(),
             'dbname' => $databaseParams->getName(),
@@ -86,36 +67,6 @@ class MysqlConnectionFactory implements ConnectionFactory
         ];
 
         return new Connection($params, $driver);
-    }
-
-    private function createDriver(): Driver
-    {
-        return new \Doctrine\DBAL\Driver\PDO\MySQL\Driver();
-
-        $driverName = $this->config->get('database.driver') ?? 'pdo_mysql';
-
-        $driverClass = $this->driverClassNameMap[$driverName] ?? null;
-
-        if (!$driverClass) {
-            throw new RuntimeException('Unknown database driver.');
-        }
-
-        return new $driverClass();
-    }
-
-    private function handlePlatform(AbstractPlatform $platform): AbstractPlatform
-    {
-        $reflect = new ReflectionClass($platform);
-
-        $platformClass = $reflect->getShortName();
-
-        if (isset($this->customPlatformClassNameMap[$platformClass])) {
-            $className = $this->customPlatformClassNameMap[$platformClass];
-
-            //return new $className();
-        }
-
-        return $platform;
     }
 
     private function getFullDatabaseVersion(): ?string
